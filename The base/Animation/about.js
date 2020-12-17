2637a9127b3005fd82cc0e4093b23808fdd4e2df
Tommy Hoang <tommyyhoangg@gmail.com>
2020-12-18 00:01:21 +0100

    Added more animations

--- The base/Animation/about.js
+++ The base/Animation/about.js
@@ -7,8 +7,15 @@ burger.addEventListener("click", () => {
    burger.classList.toggle("openB");
 });
 
-let navMenu = document.querySelector(".nav-menu");
+// Animations
+const tl = gsap.timeline({defaults: {ease: "power1.out"}});
+const tl2 = gsap.timeline({defaults: {ease: "power1.out"}});
 
-navMenu.addEventListener("click", () => {
-   navMenu.classList.toggle("menuClose");
-});
+// Navbar
+tl.from(".nav", {y: "-100%", duration: 0.80, opacity: 0});
+tl.from(".nav h1, .nav-ul li", {y: "-100%", duration: 0.70, opacity: 0, stagger: 0.1});
+
+// Landing page
+tl.from(".a-lp h1, .a-lp p, .a-lp button", {y: "100%", duration: 0.80, opacity: 0, stagger: 0.25});
+tl.from(".a-lp img", {y: "25%", duration: 1, opacity: 0},"-=0.5");
+tl2.to(".a-scroll", {y: "-3%", repeat: "-1", yoyo: true, delay: 1});
--- The base/Animation/script.js
+++ The base/Animation/script.js
@@ -16,7 +16,7 @@ tl.from(".nav h1, .nav-ul li", {y: "-100%", duration: 0.70, opacity: 0, stagger:
 
 // Landing page
 tl.from(".lp h1, .lp p, .lp button", {y: "100%", duration: 0.80, opacity: 0, stagger: 0.25});
-tl.from(".lp-img", {y: "25%", duration: 1, opacity: 0});
+tl.from(".lp-img", {y: "25%", duration: 1, opacity: 0},"-=0.5");
 
 // About section
 const about = gsap.timeline({
@@ -27,9 +27,16 @@ const about = gsap.timeline({
 });
 
 about.from(".about-rect", {x: "-75%", duration: 1, opacity: 0});
-about.from(".about h1, .about h3, .about p, .about-button", {y: "100%", duration: 1, opacity: 0, stagger: 0.1}, "-=0.5");
+about.from(".about h1, .about h3, .about p, .about-button", {y: "100%", duration: 1, opacity: 0, stagger: 0.1}, "-=0.20");
 
 // What we do section
+const wwdImg = gsap.timeline({
+   scrollTrigger: {
+      trigger: ".wwd-img",
+      start: "center bottom"
+   }
+});
+
 const wwd = gsap.timeline({
    scrollTrigger: {
       trigger: ".section-4",
@@ -37,9 +44,9 @@ const wwd = gsap.timeline({
    }
 });
 
-wwd.from(".wwd-img", {x: "75%", duration: 1, opacity: 0});
-wwd.from(".wwd-rect", {y: "75%", opacity: 0, duration: 1}, "-=0.4");
-wwd.from(".wwd h1, .wwd h3, .wwd-text1, .wwd-text2", {x: "-75%", opacity: 0, duration: 1, stagger: 0.15}, "-=0.5");
+wwdImg.from(".wwd-img", {x: "75%", duration: 1, opacity: 0});
+wwd.from(".wwd-rect", {y: "75%", opacity: 0, duration: 1, delay: 0.25});
+wwd.from(".wwd h1, .wwd h3, .wwd-text1, .wwd-text2", {x: "-75%", opacity: 0, duration: 1, stagger: 0.15, delay: 0.20});
 
 // Menu section
 const menu = gsap.timeline({
