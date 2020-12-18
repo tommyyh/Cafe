73cfbf43798ed23c373e4930c35e1e8852d27608
Tommy Hoang <tommyyhoangg@gmail.com>
2020-12-18 09:45:56 +0100

    Added animations - reservation page

--- The base/Animation/about.js
+++ The base/Animation/about.js
@@ -16,7 +16,7 @@ tl.from(".nav", {y: "-100%", duration: 0.80, opacity: 0});
 tl.from(".nav h1, .nav-ul li", {y: "-100%", duration: 0.70, opacity: 0, stagger: 0.1});
 
 // Landing page
-tl.from(".a-lp h1, .a-lp p, .a-lp button", {y: "100%", duration: 0.80, opacity: 0, stagger: 0.25});
+tl.from(".a-lp h1, .a-lp p", {y: "100%", duration: 0.80, opacity: 0, stagger: 0.25});
 tl.from(".a-lp img", {y: "25%", duration: 1, opacity: 0},"-=0.5");
 tl2.to(".a-scroll", {y: "-3%", repeat: "-1", yoyo: true, delay: 1});
 
--- The base/Animation/reservation.js
+++ The base/Animation/reservation.js
@@ -7,8 +7,38 @@ burger.addEventListener("click", () => {
    burger.classList.toggle("openB");
 });
 
-let navMenu = document.querySelector(".nav-menu");
+// Animations
+const tl = gsap.timeline({defaults: {ease: "power1.out"}});
+const tl2 = gsap.timeline({defaults: {ease: "power1.out"}});
 
-navMenu.addEventListener("click", () => {
-   navMenu.classList.toggle("menuClose");
+// Navbar
+tl.from(".nav", {y: "-100%", duration: 0.80, opacity: 0});
+tl.from(".nav h1, .nav-ul li", {y: "-100%", duration: 0.70, opacity: 0, stagger: 0.1});
+
+// Landing page
+tl.from(".r-lp-img", {y: "-50%", duration: 1, opacity: 0});
+tl.from(".r-lp h1, .r-lp p", {y: "100%", duration: 0.80, opacity: 0, stagger: 0.25});
+tl.from(".r-lp img", {y: "25%", duration: 1, opacity: 0},"-=0.5");
+tl2.to(".r-scroll", {y: "-3%", repeat: "-1", yoyo: true, delay: 1});
+
+// Reservation
+const reservation = gsap.timeline({
+   scrollTrigger: {
+      trigger: ".section-r3",
+      start: "center bottom"
+   }
 });
+
+reservation.from(".reservation-rect", {x: "75%", duration: 1, opacity: 0});
+reservation.from(".reservation h1, .reservation h3, .reservation input, .reservation button", {y: "75%", duration: 1, opacity: 0, stagger: 0.15});
+
+// Footer
+const footer = gsap.timeline({
+   scrollTrigger: {
+      trigger: ".footer-info",
+      start: "center bottom"
+   }
+});
+
+footer.from(".footer-info h1", {y: "50%", duration: 1, opacity: 0, stagger: 0.25});
+footer.from(".footer-f h1, .go-back", {y: "50%", duration: 1, opacity: 0, stagger: 0.25}, "-=0.5");
